1f2b2f220c48c374b7f59c5224908f488ac2e564
kalaakash <kalaaki@gmail.com>
2020-11-16 15:52:08 +0000

    Added commands for bot V player and 2 players

--- run_1P_1bot.sh
+++ run_1P_1bot.sh
@@ -1,1 +1,1 @@
-java -jar ManKalah.jar "nc -l localhost 12345" "java -jar MKRefAgent.jar"
+java -jar ManKalah.jar "nc localhost 12345" "java -jar MKRefAgent.jar"
---
+++ run_2P.sh
@@ -0,0 +1,1 @@
+java -jar ManKalah.jar "nc localhost 12345" "nc localhost 9876"
